9353937034eb21eda3ce92932705d875a8f31357
Ben Hayes <b.j.hayes@qmul.ac.uk>
2023-04-10 11:11:05 +0100

    Register keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,3 +1,13 @@
+--[[ local status_ok, wk = pcall(require, "which-key") ]]
+--[[ if not status_ok then ]]
+--[[ 	return ]]
+--[[ end ]]
+local wk = require("which-key")
+
+vim.o.timeout = true
+vim.o.timeoutlen = 200
+wk.setup({})
+
 local opts = { noremap = true, silent = true }
 local term_opts = { silent = true }
 
@@ -13,26 +23,37 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Vertically centre cursor on C-u and C-d
-keymap("n", "<C-d>", "<C-d>zz", opts)
-keymap("n", "<C-u>", "<C-u>zz", opts)
+wk.register({
+	name = "Scroll",
+	["<C-d>"] = { "<C-d>zz", "Scroll down" },
+	["<C-u>"] = { "<C-u>zz", "Scroll up" },
+}, {
+	mode = "n",
+	silent = true,
+	noremap = true,
+})
 
 -- Normal --
 -- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+wk.register({
+	name = "Window navigation",
+	["<C-h>"] = { "<C-w>h", "Left" },
+	["<C-j>"] = { "<C-w>j", "Down" },
+	["<C-k>"] = { "<C-w>k", "Up" },
+	["<C-l>"] = { "<C-w>l", "Right" },
+	["<S-Up>"] = { ":resize +2<CR>", "Increase height" },
+	["<S-Down>"] = { ":resize -2<CR>", "Decrease height" },
+	["<S-Left>"] = { ":vertical resize -2<CR>", "Decrease width" },
+	["<S-Right>"] = { ":vertical resize +2<CR>", "Increase width" },
+	["<S-l>"] = { ":bnext<CR>", "Next buffer" },
+	["<S-h>"] = { ":bprevious<CR>", "Previous buffer" },
+}, {
+	mode = "n",
+	silent = true,
+	noremap = true,
+})
 
--- Resize with arrows
-keymap("n", "<S-Up>", ":resize +2<CR>", opts)
-keymap("n", "<S-Down>", ":resize -2<CR>", opts)
-keymap("n", "<S-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<S-Right>", ":vertical resize +2<CR>", opts)
-
--- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
-
+-- THESE DON'T PLAY NICELY WITH WK FOR SOME REASON --
 -- Visual --
 -- Stay in indent mode
 keymap("v", "<", "<gv", opts)
@@ -49,61 +70,108 @@ keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 
 -- Telescope
-keymap(
-  "n",
-  "<leader>f",
-  "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({previewer = true}))<cr>",
-  opts
-)
-
-keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
-
-keymap(
-  "n",
-  "<leader>s",
-  "<cmd>lua require'telescope.builtin'.lsp_document_symbols(require('telescope.themes').get_dropdown({previewer = true}))<cr>",
-  opts
-)
-
-keymap("n", "<leader>t", "<cmd>Telescope<cr>", opts)
-keymap("n", "<leader>d", "<cmd>Telescope diagnostics<cr>", opts)
-
-keymap(
-  "n",
-  "<leader>b", "<cmd>lua require'telescope.builtin'.buffers(require('telescope.themes').get_dropdown({previewer = true}))<cr>", opts
-)
-
-keymap(
-  "n",
-  "<leader>,",
-  "<cmd>lua require'telescope.builtin'.command_history(require('telescope.themes').get_dropdown({previewer = true}))<cr>",
-  opts
-)
+wk.register({
+	name = "Telescope",
+	["<leader>"] = {
+		f = {
+			"<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({previewer = true}))<cr>",
+			"Find files",
+		},
+		s = {
+			"<cmd>lua require'telescope.builtin'.lsp_document_symbols(require('telescope.themes').get_dropdown({previewer = true}))<cr>",
+			"Find symbols",
+		},
+		t = { "<cmd>Telescope<cr>", "Open Telescope" },
+		d = { "<cmd>Telescope diagnostics<cr>", "Open diagnostics for current project" },
+		b = {
+			"<cmd>lua require'telescope.builtin'.buffers(require('telescope.themes').get_dropdown({previewer = true}))<cr>",
+			"List current buffers",
+		},
+		[","] = {
+			"<cmd>lua require'telescope.builtin'.command_history(require('telescope.themes').get_dropdown({previewer = true}))<cr>",
+			"Open command history",
+		},
+	},
+}, {
+	mode = "n",
+	silent = true,
+	noremap = true,
+})
+wk.register({
+	name = "Telescope",
+	["<C-t>"] = { "<cmd>Telescope live_grep<cr>", "Find text in project" },
+}, {
+	mode = "n",
+	silent = true,
+	noremap = true,
+})
 
 -- NvimTree
-keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+wk.register({
+	name = "NvimTree",
+	["<leader>"] = {
+		e = { "<cmd>NvimTreeToggle<cr>", "Toggle NvimTree" },
+	},
+}, {
+	mode = "n",
+	silent = true,
+	noremap = true,
+})
 
 -- Formatting
-keymap("n", "<leader>F", ":Format<cr>", opts)
+wk.register({
+	name = "Format",
+	["<leader>"] = {
+		F = { "<cmd>Format<cr>", "Format buffer" },
+	},
+}, {
+	mode = "n",
+	silent = true,
+	noremap = true,
+})
 
 -- Safe delete buffers
 keymap("n", "<C-q>", ":Bd<cr>", opts)
 
--- Return to home screen (alpha)
-keymap("n", "<leader>a", ":Alpha<cr>", opts)
+wk.register({
+	name = "REPL",
+	["<leader>"] = {
+		r = {
+			name = "Iron",
+			s = { "<cmd>IronRepl<cr>", "Open Iron REPL" },
+			r = { "<cmd>IronRestart<cr>", "Restart Iron REPL" },
+			f = { "<cmd>IronFocus<cr>", "Focus Iron REPL" },
+			h = { "<cmd>IronHide<cr>", "Hide Iron REPL" },
+		},
+		c = { "o# %%\n", "Create cell" },
+		C = { "[h o# %%\n", "Create cell above" },
+		x = { "qtrih", "Run cell" },
+		X = { "qtrih]h", "Run cell and move to next" },
+		z = { "qtrgg", "Run all cells to this line" },
+		Z = { "ggqtrG", "Run all cells" },
+	},
+	["[h"] = "Previous cell",
+	["]h"] = "Next cell",
+	["]x"] = { "qtrih", "Run cell" },
+}, {
+	mode = "n",
+	silent = true,
+	noremap = false,
+})
 
--- open iron repl
-keymap('n', '<leader>rs', '<cmd>IronRepl<cr>', opts)
-keymap('n', '<leader>rr', '<cmd>IronRestart<cr>', opts)
-keymap('n', '<leader>rf', '<cmd>IronFocus<cr>', opts)
-keymap('n', '<leader>rh', '<cmd>IronHide<cr>', opts)
-
-keymap('n', '<leader>c', 'o# %%\n', term_opts)
-keymap('n', '<leader>C', '[h o# %%\n', term_opts)
-
--- run jupyter cell
-keymap("n", "]x", "qtrih", term_opts)
-keymap("n", "<leader>x", "qtrih", term_opts)
-keymap("n", "<leader>X", "qtrih]h", term_opts)
-keymap("n", "<leader>z", "qtrgg", term_opts)
-keymap("n", "<leader>Z", "ggqtrG", term_opts)
+-- Neotest
+wk.register({
+  name = "Neotest",
+  ["<leader>;"] = {
+    name = "Neotest",
+    [";"] = { "<cmd>lua require('neotest').summary.toggle()<CR>", "Open magic test panel" },
+    c = { "<cmd>lua require('neotest').run.run()<CR>", "Run current test" },
+    l = { "<cmd>lua require('neotest').run.run_last()<CR>", "Run last test" },
+    f = { "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<CR>", "Run current test file" },
+    d = { "<cmd>lua require('neotest').run.debug()<CR>", "Debug current test" },
+  },
+}, {
+  mode = "n",
+  silent = true,
+  noremap = true,
+})
